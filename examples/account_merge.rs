cf6640f26e9058a5f255ac353cd46ec11b2da84a
Vincent Amouret <vincent.amouret@gmail.com>
2025-03-10 23:03:17 +0100

    account merge

---
+++ examples/account_merge.rs
@@ -0,0 +1,38 @@
+use std::{cell::RefCell, rc::Rc};
+
+use stellar_baselib::{
+    account::{Account, AccountBehavior},
+    keypair::{Keypair, KeypairBehavior},
+    network::{NetworkPassphrase, Networks},
+    operation::Operation,
+    transaction::TransactionBehavior,
+    transaction_builder::{TransactionBuilder, TransactionBuilderBehavior},
+    xdr::{Limits, WriteXdr},
+};
+
+pub fn main() {
+    let kp = Keypair::random().unwrap();
+    println!("Secret key: {}", kp.secret_key().unwrap());
+    println!("Public key: {}", kp.public_key());
+    let sequence = "0";
+    let source_account = Rc::new(RefCell::new(
+        Account::new(&kp.public_key(), sequence).unwrap(),
+    ));
+    let network = Networks::testnet();
+    let mut builder = TransactionBuilder::new(source_account, network, None);
+
+    let destination = Keypair::random().unwrap().public_key();
+    builder
+        .fee(100u32)
+        .add_operation(Operation::account_merge(destination, None).unwrap());
+
+    let mut tx = builder.build();
+    tx.sign(&[kp]);
+
+    let xdr = tx
+        .to_envelope()
+        .unwrap()
+        .to_xdr_base64(Limits::none())
+        .unwrap();
+    println!("Account Merge XDR: {xdr}");
+}
